ac9cbdb2d3bfa79b5212f0d23506b8d6e4089718
Sean Oliver <snoliver@umich.edu>
2020-06-07 16:02:46 -0400

    started genetic chromosone class

---
+++ genetic.h
@@ -0,0 +1,16 @@
+#ifndef GENETIC_H
+#define GENETIC_H
+
+#include "board.h"
+#include <vector>
+
+class chromosone{
+    public:
+
+    private:
+        const Board board;
+        int fitness;
+        int selection_point;
+}
+
+#endif
