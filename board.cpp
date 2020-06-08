a6aba1dc6de9cc7fe5e441d07e48500f75fd32da
Sean Oliver <snoliver@umich.edu>
2020-06-08 14:13:14 -0400

    made board size templated and made random board

--- board.cpp
+++ board.cpp
@@ -1,17 +1,32 @@
 #include "board.h"
+#include <random>
+#include <algorithm>
 
-
-int board::get_n() const{
-    return grid.size();
+template <typename N>
+board<N>::board(){
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<int> distrib(1, n);
+    auto gen = [&distrib, &gen](){
+        return distrib(gen);
+    };
+    std::generate(grid.begin(), grid.end(), gen);
 }
 
-int board::at(int column_num) const{
+template <typename N>
+int board<N>::get_n() const{
+    return N;
+}
+
+template <typename N>
+int board<N>::at(int column_num) const{
     if(column_num < 0 || column_num >= grid.size()){
         throw outOfBoundsException();
     }
     return grid[column_num];
 }
 
-std::vector<int> board::get_grid() const{
+template <typename N>
+std::vector<int> board<N>::get_grid() const{
     return grid;
 }
--- board.h
+++ board.h
@@ -1,15 +1,16 @@
 #ifndef BOARD_H
 #define BOARD_H
 #include<vector>
+template <typename N>
 class board{
     public:
         board(std::vector<int> grid_in) : grid(grid_in) {}
-        
+        board();
         int get_n() const;
         int at(int column_num) const;
         std::vector<int> get_grid() const;
     private:
-        const std::vector<int> grid;
+        const std::vector<int> grid(N, -1);
 
 };
 
