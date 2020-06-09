1e2767aa822e5cf45b1701bd59fcfbadfba45bcd
Sean Oliver <snoliver@umich.edu>
2020-06-09 13:31:25 -0400

    remove template in board

--- board.cpp
+++ board.cpp
@@ -2,31 +2,24 @@
 #include <random>
 #include <algorithm>
 
-template <int N>
-board<N>::board(){
+board::board(int n){
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<int> distrib(1, n);
     auto gen = [&distrib, &gen](){
         return distrib(gen);
     };
+    grid.resize(n);
     std::generate(grid.begin(), grid.end(), gen);
 }
 
-template <int N>
-int board<N>::get_n() const{
-    return N;
+int board::get_n() const{
+    return grid.size();
 }
 
-template <int N>
-int board<N>::at(int column_num) const{
+int board::at(int column_num) const{
     if(column_num < 0 || column_num >= grid.size()){
         throw outOfBoundsException();
     }
     return grid[column_num];
-}
-
-template <int N>
-std::vector<int> board<N>::get_grid() const{
-    return grid;
 }
--- board.h
+++ board.h
@@ -1,16 +1,14 @@
 #ifndef BOARD_H
 #define BOARD_H
 #include<vector>
-template <int N>
 class board{
     public:
         board(std::vector<int> grid_in) : grid(grid_in) {}
-        board();
+        board(int n);
         int get_n() const;
         int at(int column_num) const;
-        std::vector<int> get_grid() const;
     private:
-        const std::vector<int> grid(N, -1);
+        std::vector<int> grid;
 
 };
 
