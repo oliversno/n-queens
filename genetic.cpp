79362bda198898e5a814b93ce79051997e49f66b
Sean Oliver <snoliver@umich.edu>
2020-06-09 13:33:02 -0400

    made genetic algorithm function

--- genetic.cpp
+++ genetic.cpp
@@ -2,11 +2,16 @@
 #include <iostream>
 using namespace std;
 
+void genetic_algorithm(vector<chromosone>& chromosones){
+    
+}
+
 int main(int argc, char** argv){
     if(argc != 3){
         cerr << "Incorrect number of arguments: ./genetic N K\n";
     }
     int n = atoi(argv[1]);
     int k = atoi(argv[2]);
-    vector<chromosone> chromosones(k, chromosone( board<n>() )); 
+    vector<chromosone> chromosones(k, chromosone( board(n) ));
+    genetic_algorithm(chromosones);
 }
