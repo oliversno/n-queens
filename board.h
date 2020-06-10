a37a58a6924adbcc419e019b78dc46b7e989cd90
Sean Oliver <snoliver@umich.edu>
2020-06-10 13:39:01 -0400

    added output board

--- board.h
+++ board.h
@@ -1,12 +1,31 @@
 #ifndef BOARD_H
 #define BOARD_H
 #include<vector>
+#include <iostream>
+
 class board{
     public:
         board(std::vector<int> grid_in) : grid(grid_in) {}
         board(int n);
         int get_n() const;
         int at(int column_num) const;
+
+        friend std::ostream &operator<<( std::ostream &output, const board &B ){
+            const int n = B.get_n();
+            for(int column_num : B.grid){
+                for(int i = 0; i < n; ++i){
+                    if(i == column_num){
+                        output << 'Q';
+                    }
+                    else{
+                        output << '*';
+                    }
+                }
+                output << '\n';
+            }
+            return output;
+        }
+        
     private:
         std::vector<int> grid;
 
