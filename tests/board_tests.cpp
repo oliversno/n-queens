551270fcff3e30bff6a856f0bc26b7e6c9d32bcc
Sean Oliver <snoliver@umich.edu>
2020-06-12 13:16:35 -0400

    added some board ctor tests

--- tests/board_tests.cpp
+++ tests/board_tests.cpp
@@ -1,2 +1,15 @@
+#include<vector>
+
 #include <gtest/gtest.h>
 #include "include/board.h"
+
+TEST(BoardTests, InitRandom){
+    Board b(3);
+    SUCCEED();
+}
+
+TEST(BoardTests, InitFromVector){
+    std::vector<int> v{1,2,3};
+    Board b(v);
+    SUCCEED();
+}
