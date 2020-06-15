6845b2a951e2e5db8bc38cc869a1a356f89c17c3
Sean Oliver <snoliver@umich.edu>
2020-06-15 12:23:33 -0400

    added board tests

--- tests/board_tests.cpp
+++ tests/board_tests.cpp
@@ -13,3 +13,14 @@ TEST(BoardTests, InitFromVector){
     Board b(v);
     SUCCEED();
 }
+
+TEST(BoardTest, get_n_one){
+    Board b(5);
+    EXPECT_EQ(b.get_n(), 5);
+}
+
+TEST(BoardTest, get_n_two){
+    std::vector<int> v{4,5,6,7};
+    Board b(v);
+    EXPECT_EQ(b.get_n(), 4);
+}
