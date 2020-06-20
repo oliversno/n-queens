dde740b425992f9dd381408829f41dc7f99bd5a3
Sean Oliver <snoliver@umich.edu>
2020-06-20 17:03:02 -0400

    started chromosone tests

--- tests/chromosone_tests.cpp
+++ tests/chromosone_tests.cpp
@@ -2,6 +2,10 @@
 #include "include/chromosone.h"
 
 
-TEST(ChromosoneTests, Placeholder){
-    SUCCEED();
+TEST(ChromosoneTests, getBoard){
+    std::vector<int> v{2,3,1};
+    Board b{v};
+    chromosone c{b};
+    EXPECT_EQ(b, c.get_board());
 }
+
