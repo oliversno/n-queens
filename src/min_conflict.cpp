0947ed96bc92acb000eaa716a0079c51d3a0f8f4
Sean Oliver <snoliver@umich.edu>
2020-08-14 13:35:27 -0400

    added printing steps taken

--- src/min_conflict.cpp
+++ src/min_conflict.cpp
@@ -5,20 +5,21 @@
 #include <cmath>
 using namespace std;
 
-void min_conflict(Board& board, int max_steps){
+int min_conflict(Board& board, int max_steps){
     std::random_device rd;
     std::mt19937 gen(rd());
     const int n = board.get_n();
     std::uniform_int_distribution<> distrib(0, n-1);
     for(int i = 0; i < max_steps; i++){
         if(board.get_conflicts() == 0){ // solution found
-            return;
+            return i;
         }
         int row = distrib(gen);
         vector<int> conflict_row = board.get_conflicts(row);
         int column = min_element(conflict_row.begin(), conflict_row.end()) - conflict_row.begin();
         board.set_queen(row, column);
     }
+    return max_steps;
 }
 
 int main(int argv, char **argc){
@@ -28,8 +29,10 @@ int main(int argv, char **argc){
     int n = atoi(argc[1]);
     int max_steps = atoi(argc[2]);
     Board board{n};
-    min_conflict(board, n);
+    int steps_taken = min_conflict(board, max_steps);
     cout << "FINAL\n";
     cout << board;
+    cout << "# Conflicts: " << board.get_conflicts() << '\n';
+    cout << steps_taken << " steps taken\n";
 
 }
