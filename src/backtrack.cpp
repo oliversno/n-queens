0877b3da106742c5935bb6dc867988773c7d7972
Sean Oliver <snoliver@umich.edu>
2020-08-16 12:55:38 -0400

    finished backtracking

--- src/backtrack.cpp
+++ src/backtrack.cpp
@@ -16,11 +16,38 @@ bool is_safe(const Board& board, int row, int col){
     return true;
 }
 
+bool backtrack_util(Board board, int col){
+    const int n = board.get_n();
+    if(col >= n){
+        return true;
+    }
+    for(int i = 0; i < n; ++i){
+        if(is_safe(board, i, col)){
+            board.set_queen(i, col);
+            if(backtrack_util(board, col + 1)){
+                return true;
+            }
+            board.remove_queen(i);
+        }
+    }
+    return false; //queen can't be safely placed, backtrack
+}
+
+bool backtrack(Board& board){
+    return backtrack_util(board, 0);
+}
+
 int main(int argv, char **argc){
     if(argv != 2){
         cerr << "Incorrect number of arguments: ./backtrack SIZE_OF_BOARD\n";
     }
     int n = atoi(argc[1]);
     Board board{n};
-
+    if(backtrack(board)){
+        cout <<"Solution Found:\n";
+        cout << board; 
+    }
+    else{
+        cout << "No Solution Exists for N=" << n << '\n';
+    }
 }
